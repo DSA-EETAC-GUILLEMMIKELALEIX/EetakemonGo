7b5c5158bb9eedf996d1e781b7b076ed51d05b47
aleix <aleix11fcb@gmail.com>
2017-03-13 13:15:46 +0100

    Actualización

--- src/Modelo/Eetakemon.java
+++ src/Modelo/Eetakemon.java
@@ -1,28 +1,43 @@
 package Modelo;
+
+import javafx.scene.image.Image;
+
 // Clase que define y permite crear objetos Eetakemon
 public class Eetakemon {
     private String nombre;
-    private int id;
+    private String tipo;
+    private Image foto;
     private int nivel;
+    private String ataque;
 
-    public Eetakemon(String name, int id, int level)
+    public Eetakemon(String name, String tipo, Image foto, int level, String ataque)
     {
         this.nombre = name;
-        this.id= id;
+        this.tipo = tipo;
+        this.foto = foto;
         this.nivel=level;
+        this.ataque=ataque;
     }
 
     //Obtener nombre
     public String getNombre() {
         return nombre;
     }
-    //Obtener ID
-    public int getId() {
-        return id;
+    //Obtener Tipo
+    public String getTipo() {
+        return tipo;
+    }
+    //Obtener foto
+    public Image getFoto() {
+        return foto;
     }
     //Obtener Nivel
     public int getNivel() {
         return nivel;
-    }//
+    }
+    //Obtener ataque
+    public String getAtaque() {
+        return ataque;
+    }
 }
 
