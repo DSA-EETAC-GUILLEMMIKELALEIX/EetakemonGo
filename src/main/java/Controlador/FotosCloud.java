3df8136c8b65f34b951353d7142d562847dd609e
guillem74 <guillemeetac@gmail.com>
2017-04-26 19:13:22 +0200

    Cloudinary

---
+++ src/main/java/Controlador/FotosCloud.java
@@ -0,0 +1,11 @@
+package Controlador;
+//import com.cloudinary.StoredFile;
+//import javax.persistence.*;
+//import java.util.Date;
+
+//@Entity(name = "photos")
+/**
+ * Created by usuario on 26/04/2017.
+ */
+//public class FotosCloud {
+//}
