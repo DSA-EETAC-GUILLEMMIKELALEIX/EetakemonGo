b3e9882d5ce91b2a092884db0bf7df8ecaad7711
guillem74 <guillemeetac@gmail.com>
2017-05-16 18:34:43 +0200

    capturando

--- src/main/java/Modelo/Relation/RelationDAO.java
+++ src/main/java/Modelo/Relation/RelationDAO.java
@@ -2,6 +2,10 @@ package Modelo.Relation;
 
 import Modelo.DAO.DAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class RelationDAO extends DAO {
@@ -11,18 +15,43 @@ public class RelationDAO extends DAO {
     protected boolean updateRelation(){
         return update();
     }
-    protected void selectEetakemonById(int id){
+    protected void selectRelationById(int id){
         select(id);
     }
-    protected void deleteEetakemon(){
+    protected void deleteRelation(){
         delete();
     }
-    protected List findAllEetakemons(){
+    protected List findAllRelations(){
         return findAll();
     }
-    protected boolean checkEetakemonExistent(String nombre){
-        return checkExistent("nombre",nombre);
+
+    protected boolean checkRelationExistent(int idUser, int idEetakemon){
+        boolean existent = false;
+        Connection con = getConnection();
+        StringBuffer query = new StringBuffer("SELECT * FROM ");
+        query.append(this.getClass().getSimpleName());
+        query.append(" WHERE idUser='" + idUser + "' AND idEetakemon='" + idEetakemon + "';");
+        try {
+            PreparedStatement ps = con.prepareStatement(query.toString());
+            ResultSet rs = ps.executeQuery();
+
+            if (!rs.next()) {
+                logger.info("INFO: Eetakemon ya capturado");
+                existent = false;
+
+            } else {
+                logger.info("INFO: Nuevo Eetakemon capturado");
+                existent = true;
+            }
+            ps.close();
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return existent;
     }
+
+
 
 }
 
--- src/main/java/Modelo/Relation/RelationManager.java
+++ src/main/java/Modelo/Relation/RelationManager.java
@@ -1,5 +1,42 @@
 package Modelo.Relation;
+import org.apache.log4j.Logger;
+
+import java.util.List;
 
 
 public class RelationManager {
+    private final static Logger logger = Logger.getLogger(Modelo.Relation.RelationManager.class);//
+    
+
+    public Relation getRelationById(int id){
+        Relation e= new Relation();
+        e.selectRelationById(id);
+
+        return e;
+    }
+
+    public boolean addRelation(Relation e){
+        Boolean exist=false;
+        exist=e.checkRelationExistent(e.getIdUser(),e.getIdEetakemon());
+        if(!exist){
+            e.insertRelation();
+        }
+        return exist;
+    }
+
+    public Relation deleteRelation(int id){
+        Relation e = new Relation();
+        e.selectRelationById(id);
+        e.deleteRelation();
+
+        return e;
+    }
+
+    public List listAllRelation(){
+        List<Relation> list;
+        list = new Relation().findAllRelations();
+
+        return list;
+    }
+
 }
