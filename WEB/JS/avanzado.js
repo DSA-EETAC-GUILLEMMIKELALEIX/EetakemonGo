7bdbab50d66cc8de79021c7e56ee9548c53b6a31
Soy programador <mikel.ga22@gmail.com>
2017-05-28 11:11:15 +0200

    Cambios web

--- WEB/JS/avanzado.js
+++ WEB/JS/avanzado.js
@@ -62,13 +62,15 @@ $(document).ready(function(){
                 console.log(result);
                 $.each(result, function (i, obj) {
                     console.log((obj));
-                    $("#tabla-eetakemon").append("<tr class=\"eetakemon\" onclick='webEetacemon("+obj.id+")'>" +
+                    $("#tabla-eetakemon").append("<tr class=\"eetakemon\">" +
                         "<td>" +
                         "<img src = \" /images/" + obj.nombre + ".png\" style=\"width:50px;height:50px;\" ' >" +
                         "<td>" +obj.id+ "</td>"+
                         "<td>" +obj.nombre+ "</td>"+
                         "<td>" +obj.tipo+ "</td>"+
                         "<td>" +obj.nivel+ "</td>"+
+                        "<td class=\"edit\">" +"Editar"+ "</td>"+
+                        "<td class=\"delete\">" +"Borrar"+ "</td>"+
                         "</tr>");
                 });
             },
@@ -92,6 +94,7 @@ $(document).ready(function(){
         $('.opciones-usuario').show();
     });
 
+    //change user-eetakemon options
     $("#opcion-eetakemon").click(function(){
         $('#opcion-usuario').removeAttr('style');
         $(this).css({"background-color": "#636363","color":"white"});
