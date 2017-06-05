696cbf61efe406af0263e0f3a6887b9ff570a8d6
aleix <aleix11fcb@gmail.com>
2017-06-05 23:29:28 +0200

    Actualització: Classe Capturar

--- src/main/java/Controller/EetakemonService.java
+++ src/main/java/Controller/EetakemonService.java
@@ -1,15 +1,18 @@
 package Controller;
 
+import Model.Capturar.Capturar;
 import Model.Eetakemon.Eetakemon;
 import Model.Eetakemon.EetakemonManager;
 import Model.Exceptions.NotSuchPrivilegeException;
 import Model.Exceptions.UnauthorizedException;
+import Model.Location.Location;
+import Model.Location.LocationManager;
 import Model.Security.AuthenticationManager;
 
 import javax.inject.Singleton;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
-import java.util.List;
+import java.util.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import javax.imageio.ImageIO;
@@ -128,20 +131,88 @@ public class EetakemonService {
 
 
     //Eetakemon nivel normal
-    @GET
-    @Path("/tipo")
+    @POST
+    @Path("/Tipo")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getEetakemonTipo(@Context HttpHeaders header, String tipo) {
+    public Response getEetakemonTipo(Eetakemon eetak) {
+        System.out.println("holalalalalalala");
         Eetakemon e = new Eetakemon();
-        try {
-            e = manager.getEetakemonByType(header, tipo);
-            if (e.getNombre() != null) {
-                return Response.status(Response.Status.OK).entity(e).build();//200
-            } else {
-                return Response.status(Response.Status.NO_CONTENT).entity("No eetakemon found").build();//204
-            }
-        }catch(UnauthorizedException ex){
-            return Response.status(Response.Status.UNAUTHORIZED).entity("Unauthorized").build();//401
+        e=manager.getEetakemonByType(eetak.getTipo());
+        System.out.println(e);
+        if (e.getNombre()!=null) {
+            return Response.status(201).entity(e).build();
         }
+        else{
+            return Response.status(202).entity("No se ha podido visualizar el usuario: ").build();
+        }
+    }
+
+    //Lista de eetac-emons
+    @GET
+    @Path("/ListApp")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response ListarEetakemons() {
+        List<Eetakemon> list;
+        System.out.println("AAAAAA");
+            list = manager.listAllEetakemon();
+            if (!list.isEmpty()) {
+                GenericEntity<List<Eetakemon>> entity;
+                entity = new GenericEntity<List<Eetakemon>>(list) {
+                };
+                System.out.println(entity);
+                return Response.status(Response.Status.OK).entity(entity).build();//200
+            } else {
+                return Response.status(Response.Status.NO_CONTENT).entity("No content").build();//204
+            }
+
+    }
+
+    //Lista de eetac-emons
+    @GET
+    @Path("/ListMapa")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response ListarMapaEetakemons() {
+        List<Capturar> liste = new ArrayList<Capturar>();
+
+        Eetakemon e = new Eetakemon();
+        Capturar capt = new Capturar();
+        Location loc= new Location();
+        
+        for (int i=0;i<3;i++) {
+            e = manager.getEetakemonByType("Inferior");
+            capt.setNombre(e.getNombre());
+            capt.setTipo(e.getTipo());
+            //loc = locat();  Locat es una funcio del LocationManager
+            capt.setLatLong(loc);
+            System.out.println(e);
+            liste.add(i, capt);
+        }
+        e = manager.getEetakemonByType("Normal");
+        capt.setNombre(e.getNombre());
+        capt.setTipo(e.getTipo());
+        //loc = locat();  Locat es una funcio del LocationManager
+        capt.setLatLong(loc);
+        System.out.println(e);
+        liste.add(capt);
+
+        e = manager.getEetakemonByType("Legendario");
+        capt.setNombre(e.getNombre());
+        capt.setTipo(e.getTipo());
+        //loc = locat();  Locat es una funcio del LocationManager
+        capt.setLatLong(loc);
+        System.out.println(e);
+        liste.add(capt);
+
+        System.out.println("list:"+liste.size());
+
+        if (!liste.isEmpty()) {
+            GenericEntity<List<Capturar>> entity;
+            entity = new GenericEntity<List<Capturar>>(liste) {
+            };
+            return Response.status(200).entity(entity).build();//200
+        } else {
+            return Response.status(Response.Status.NO_CONTENT).entity("No content").build();//204
+        }
+
     }
 }
---
+++ src/main/java/Model/Location/LocationManager.java
@@ -0,0 +1,73 @@
+package Model.Location;
+
+import Model.Location.Location;
+
+import java.util.Random;
+
+/**
+ * Created by aleix on 05/06/2017.
+ */
+public class LocationManager {
+
+    /*LatLng aa = new LatLng(41.27539318720677, 1.9851908683449437); //Biblioteca
+    LatLng bb = new LatLng(41.274566700768275, 1.9851908683449437);//Residencia
+    LatLng cc = new LatLng(41.275224665468244, 1.986177653066079); //Entrada EETAC-1
+    LatLng dd = new LatLng(41.275561305325134, 1.9871539771884272);//Entrada EETAC-2
+    LatLng ee = new LatLng(41.27564395319903, 1.9865638912051509); //Entrada ESAB
+    LatLng ff = new LatLng(41.27557178752102, 1.9858227968461506); //Canasta Basquet
+    LatLng gg = new LatLng(41.275515250643224, 1.9840220282936217); //Pont
+    LatLng hh = new LatLng(41.27581166751877, 1.9877861738041247); //Edifici professors
+    LatLng ii = new LatLng(41.27523514765666, 1.9881053566871287); //Entrada UOC
+    LatLng jj = new LatLng(41.275731035685105, 1.989977538569292); //Parking*/
+
+    private Location aa = new Location(41.27539318720677, 1.9851908683449437);
+    private Location bb = new Location(41.274566700768275, 1.9851908683449437);
+    private Location cc = new Location(41.275224665468244, 1.986177653066079);
+    private Location dd = new Location(41.275561305325134, 1.9871539771884272);
+    private Location ee = new Location(41.27564395319903, 1.9865638912051509);
+    private Location ff = new Location(41.27557178752102, 1.9858227968461506);
+    private Location gg = new Location(41.275515250643224, 1.9840220282936217);
+    private Location hh = new Location(41.27581166751877, 1.9877861738041247);
+    private Location ii = new Location(41.27523514765666, 1.9881053566871287);
+    private Location jj = new Location(41.275731035685105, 1.989977538569292);
+
+
+    public Location locat() {
+        Location locat = new Location();
+        Random rand = new Random();
+        int n = rand.nextInt(9);
+
+        if(n==0){
+            locat=aa;
+        }
+        if(n==1){
+            locat=bb;
+        }
+        if(n==2){
+            locat=cc;
+        }
+        if(n==3){
+            locat=dd;
+        }
+        if(n==4){
+            locat=ee;
+        }
+        if(n==5){
+            locat=ff;
+        }
+        if(n==6){
+            locat=gg;
+        }
+        if(n==7){
+            locat=hh;
+        }
+        if(n==8){
+            locat=ii;
+        }
+        if(n==9){
+            locat=jj;
+        }
+
+        return locat;
+    }
+}
