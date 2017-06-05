ddb30fa451ee80bf083eb185f59f655e333e479f
guillem74 <guillemeetac@gmail.com>
2017-06-05 23:27:05 +0200

    Questions

---
+++ src/main/java/Model/Questions/Question.java
@@ -0,0 +1,52 @@
+package Model.Questions;
+
+/**
+ * Created by usuario on 05/06/2017.
+ */
+public class Question {
+    private int id;
+    private String level;
+    private String question;
+    private int answer;
+
+    public Question(){}
+
+    public Question(int id, String level, String question, int answer){
+        this.id=id;
+        this.level=level;
+        this.question=question;
+        this.answer=answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+}
