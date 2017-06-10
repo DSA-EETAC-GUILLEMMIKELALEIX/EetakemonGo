582e4848dfca7ddb56a876cf53fccc34c2a6a791
Mikel <mikel.ga22@gmail.com>
2017-06-10 12:22:38 +0200

    Create insertarEekamon.sql

--- insertarEekamon.sql
+++ insertarEekamon.sql
@@ -1,1 +1,9 @@
+USE proyecto;
 
+Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES (Bernorlax, Normal, 15, http://localhost:8081/images/bernorlax.png);
+Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES (Davyphno, Normal, 15, http://localhost:8081/images/davyphno.png);
+Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES (Francerpie, Inferior, 1, http://localhost:8081/images/francerpie.png);
+Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES (Jesuskou, Inferior, 1, http://localhost:8081/images/jesuscou.png);
+Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES (Jordinine, Normal, 15, http://localhost:8081/images/jordinine.png);
+Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES (Lluiscarp, Legendario, 30, http://localhost:8081/images/lluiscarp.png);
+Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES (Mewdecerio, Legendario, 30, http://localhost:8081/images/mewdecerio.png);
