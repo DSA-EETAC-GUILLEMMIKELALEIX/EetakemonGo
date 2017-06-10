34badbfcaf1dfe96b4293ae1f01da0d5b8998da5
Mikel <mikel.ga22@gmail.com>
2017-06-10 12:28:21 +0200

    Create insertarEekamon.sql

--- insertarEekamon.sql
+++ insertarEekamon.sql
@@ -1,6 +1,6 @@
 USE proyecto;
 
-Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES '(Bernorlax', 'Normal', 15, 'http://localhost:8081/images/bernorlax.png');
+Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Bernorlax', 'Normal', 15, 'http://localhost:8081/images/bernorlax.png');
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Davyphno', 'Normal', 15, 'http://localhost:8081/images/davyphno.png');
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Francerpie', 'Inferior', 1, 'http://localhost:8081/images/francerpie.png');
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Jesuskou', 'Inferior', 1, 'http://localhost:8081/images/jesuscou.png');
