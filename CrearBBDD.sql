43f0613badbff72abaaa5c0bb9bbfb5229cca5b1
Soy programador <mikel.ga22@gmail.com>
2017-06-10 14:09:46 +0200

    Quitado alert en loggin/resiter

--- CrearBBDD.sql
+++ CrearBBDD.sql
@@ -42,7 +42,7 @@ Insert INTO User (nombre, contrasena, email, admin) VALUES ('guillem', 'AT0D6sMG
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Bernorlax', 'Normal', 15, 'http://localhost:8081/images/bernorlax.png');
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Davyphno', 'Normal', 15, 'http://localhost:8081/images/davyphno.png');
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Francerpie', 'Inferior', 1, 'http://localhost:8081/images/francerpie.png');
-Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Jesuskou', 'Inferior', 1, 'http://localhost:8081/images/jesuskou.png');
+Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Jesuskou', 'Inferior', 1, 'http://localhost:8081/images/jesuscou.png');
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Jordinine', 'Normal', 15, 'http://localhost:8081/images/jordinine.png');
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Lluiskarp', 'Legendario', 30, 'http://localhost:8081/images/lluiskarp.png');
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Mewdecerio', 'Legendario', 30, 'http://localhost:8081/images/mewdecerio.png');
