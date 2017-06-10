158aff2b741dae79281d2fafe0b37d10f3f854f8
Mikel <mikel.ga22@gmail.com>
2017-06-10 12:31:39 +0200

    Create insertarEekamon.sql

--- insertarEekamon.sql
+++ insertarEekamon.sql
@@ -1,5 +1,10 @@
 USE proyecto;
 
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE realtion;
+TRUNCATE eetakemon;
+SET FOREIGN_KEY_CHECKS = 1;
+
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Bernorlax', 'Normal', 15, 'http://localhost:8081/images/bernorlax.png');
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Davyphno', 'Normal', 15, 'http://localhost:8081/images/davyphno.png');
 Insert INTO Eetakemon (nombre, tipo, nivel, foto) VALUES ('Francerpie', 'Inferior', 1, 'http://localhost:8081/images/francerpie.png');
