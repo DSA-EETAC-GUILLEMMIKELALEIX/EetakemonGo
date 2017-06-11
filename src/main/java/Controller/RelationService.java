19e3b8f4f43e09a9f92e1586771ed58a0cb44968
Soy programador <mikel.ga22@gmail.com>
2017-06-11 21:04:55 +0200

    correccion relation manager

--- src/main/java/Controller/RelationService.java
+++ src/main/java/Controller/RelationService.java
@@ -23,8 +23,6 @@ public class RelationService {
         manager=new RelationManager();
     }
 
-//aaa
-
     //añadir eetakemon capturado
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -78,6 +76,8 @@ public class RelationService {
         }catch (UnauthorizedException ex) {
             return Response.status(Response.Status.UNAUTHORIZED).entity("Unauthorized").build();//401
 
+        }catch(NotSuchPrivilegeException ex){
+            return Response.status(Response.Status.FORBIDDEN).entity("Forbidden").build();//403
         }
 
     }
