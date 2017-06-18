9ceef1330e09380fa42a369ee778444f516c1a6e
Soy programador <mikel.ga22@gmail.com>
2017-06-18 15:04:30 +0200

    email

--- src/main/java/Model/User/UserManager.java
+++ src/main/java/Model/User/UserManager.java
@@ -293,7 +293,7 @@ public class UserManager {
                 props.put("mail.smtp.auth", "true");
                 props.put("mail.smtp.starttls.enable", "true");
                 props.put("mail.smtp.host", "smtp.gmail.com");
-                props.put("mail.smtp.port", "8080");
+                props.put("mail.smtp.port", "587");
 
                 Session session = Session.getInstance(props,
                         new javax.mail.Authenticator() {
