27a02fdc5ed7906deab7ca442558d8ea154f88b8
Aleix <aleixcanovas@hotmail.com>
2017-06-18 14:33:41 +0200

    Update UserManager.java

--- src/main/java/Model/User/UserManager.java
+++ src/main/java/Model/User/UserManager.java
@@ -293,7 +293,7 @@ public class UserManager {
                 props.put("mail.smtp.auth", "true");
                 props.put("mail.smtp.starttls.enable", "true");
                 props.put("mail.smtp.host", "smtp.gmail.com");
-                props.put("mail.smtp.port", "587");
+                props.put("mail.smtp.port", "8080");
 
                 Session session = Session.getInstance(props,
                         new javax.mail.Authenticator() {
